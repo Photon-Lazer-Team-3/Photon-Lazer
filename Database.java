12abbc49765d248f7fb5c49313d1d3a1131d3974
AlexJPrill <alex.j.prill@gmail.com>
2023-02-10 11:10:47 -0600

    Fixed reamianing erros

--- Database.java
+++ Database.java
@@ -6,8 +6,6 @@ public class Database
 {
 
     private Connection connection;
-    private final int id = 1;
-    private final int codeName = 4;
     private static final String URL = "";
 
 
@@ -32,17 +30,17 @@ public class Database
     }
 
     //Gets the userName from the table of players from a given id
-    public String getCodeName(int id)
+    public String getCodeName(int id) throws SQLException
     {
-        String userName = "";
         Statement stmt = connection.createStatement();
         //Why does it not like this thing here it should like it and I don't know why it does not
         ResultSet result = stmt.executeQuery("SELECT * FROM PLAYER WHERE ID " + id);
         //
         //If the query returns us a value then we
-        if(!result.wasNull())
+        if(result.getString("codename") == null)
         {
-            return result.get(codeName);
+            //Hopefully this here we cna fix sooner than latter this should be fun to finish up
+            return result.getString("codename");
         }
         else
         {
@@ -50,40 +48,37 @@ public class Database
         }
     }
 
-    //Adds a new player to the table with unique id and own code Name
-    public void addCodeName(int id, String codeName)
-    {
-
-    }
-
-    public boolean idExist(int id)
+    public boolean idExist(int id) throws SQLException
     {
         Statement statement = connection.createStatement();
         ResultSet result = statement.executeQuery("SELECT * FROM PLAYER WHERE ID " + id);
-        if(result.get(1) == null)
+        //What is the return value of no i
+        if(result.getInt("id") == 0)
         {
             return false;
         }
-        if(result.get(1) == id)
-        {
+        if(result.getInt("id") == id)
             return true;
-        }
+        else
+            return false;
     }
 
-    public void insertPlayer(int id, String codeName)
+    public void insertPlayer(int id, String codeName) throws SQLException
     {
         Statement statement = connection.createStatement();
-        statement.executeUpdate("INSTERT INTO player (" + id ", " + "null, null, " + codeName")");
+
+        statement.executeUpdate("INSTERT INTO player (" + id + ", " + "null, null, " + codeName + ")");
+     
         statement.close();
     }
 
-    public void printPlayers()
+    public void printPlayers() throws SQLException
     {
         Statement stmt = connection.createStatement();
         ResultSet result = stmt.executeQuery("SELECT * FROM PLAYER");
         while(result.next())
         {
-            System.out.println("ID: " + result.get(id) + ", code name: " + result.get(codeName));
+            System.out.println("ID: " + result.getInt("id") + ", code name: " + result.getString("codename"));
         }
 
         stmt.close();
