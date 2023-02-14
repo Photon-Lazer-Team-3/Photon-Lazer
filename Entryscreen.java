01b536695b8dcc7bd0cdf1c11122b0f87a27f835
Andrew Murphy <andrewjmurphy0309@gmail.com>
2023-02-13 18:22:03 -0600

    Adding checkboxes

--- Entryscreen.java
+++ Entryscreen.java
@@ -1,3 +1,4 @@
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import java.awt.Color;
@@ -12,24 +13,41 @@ public class Entryscreen extends JFrame {
         this.setVisible(true);
         JTextField textField = new JTextField("hiihihihihhi");
         textField.setToolTipText("Please enter some text here");*/
-        JFrame jframe = new JFrame("Entryscreen");  
-        JTextField t1,t2,t3,t4;  
-        t1=new JTextField();  
+        JFrame jFrame = new JFrame("Entryscreen");
+        JTextField t1,t2,t3,t4,t5, t6; 
+        JCheckBox c1, c2, c3;
+         
+        t1 = new JTextField();  
         t1.setBounds(50,100, 100,30);  
-        t2=new JTextField();  
+        t2 = new JTextField();  
         t2.setBounds(145,100, 200,30);
-        t3=new JTextField();  
-        t3.setBounds(50,200, 100,30);
-        t4=new JTextField();  
-        t4.setBounds(145,200, 200,30);
-        jframe.add(t1); 
-        jframe.add(t2);  
-        jframe.add(t3);
-        jframe.add(t4);
-        jframe.setSize(700,700);  
-        jframe.setLayout(null);  
-        jframe.setVisible(true);  
-        jframe.getContentPane().setBackground(Color.RED);
+        t3 = new JTextField();  
+        t3.setBounds(50,123, 100,30);
+        t4 = new JTextField();  
+        t4.setBounds(145,123, 200,30);
+        t5 = new JTextField();
+        t5.setBounds(50, 146, 100, 30);
+        t6 = new JTextField();
+        t6.setBounds(145, 146, 200, 30);
+        c1 = new JCheckBox("0");
+        c1.setBounds(30, 105, 20, 20);
+        c2 = new JCheckBox("1");
+        c2.setBounds(30, 128, 20, 20);
+        c3 = new JCheckBox("2");
+        c3.setBounds(30, 151, 20, 20);
+        jFrame.add(t1); 
+        jFrame.add(t2);  
+        jFrame.add(t3);
+        jFrame.add(t4);
+        jFrame.add(t5);
+        jFrame.add(t6);
+        jFrame.add(c1);
+        jFrame.add(c2);
+        jFrame.add(c3);
+        jFrame.setSize(700,700);  
+        jFrame.setLayout(null);  
+        jFrame.setVisible(true);  
+        jFrame.getContentPane().setBackground(new Color(255,17,0));
     }
 
     public static void main(String[] args) throws Exception{
