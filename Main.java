bdff9f5ffd0902a1ca690cdf7eb868853c59b004
AlexJPrill <alex.j.prill@gmail.com>
2023-02-17 00:17:24 -0600

    testing

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class main 
 {
-    public static void main(String [] args)
+    public static void main(String [] args) throws Exception
     {
         Database db = new Database();
     }    
