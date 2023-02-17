88984a914819d851e38368ff821a83d325b5ee26
AlexJPrill <alex.j.prill@gmail.com>
2023-02-17 00:44:08 -0600

    testing

--- Database.java
+++ Database.java
@@ -1,4 +1,3 @@
-
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class main
 {
     public static void main(String [] args) throws Exception
     {
-        Database db = new Database();
+       Database db = new Database();
+
     }    
 }
