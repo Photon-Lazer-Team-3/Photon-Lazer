4557a3d10f3511ddb678df79a3b18e86770c21c4
AlexJPrill <alex.j.prill@gmail.com>
2023-02-16 23:04:27 -0600

    testing

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import Database;
+
+
+public class main 
+{
+    public static void main(String [] args) throws Exception
+    {
+       Database db = new Database();
+        db.insertPlayer(2, "goofe2232");
+
+    }    
+
+}
