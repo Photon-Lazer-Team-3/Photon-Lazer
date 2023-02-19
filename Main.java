f23c6e9b0b8eb49827d2694ecdbc124701cfb8fb
AlexJPrill <alex.j.prill@gmail.com>
2023-02-19 17:13:11 -0600

    added implementation

--- Main.java
+++ Main.java
@@ -11,10 +11,8 @@ public class Main
         int i = 0;
         while(i < 30)
         {
-            String str = screen.greenText[i].getText();
             if(!screen.greenText[i].getText().equals(""))
             {
-                System.out.println("ID found");
                 if(db.idExist(Integer.parseInt(screen.greenText[i].getText())))
                 {
                     System.out.println("ID is in database");
