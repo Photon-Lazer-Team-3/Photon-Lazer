be4e2159dc8ca5ab21109611a62c89d2ad0c6b26
AlexJPrill <alex.j.prill@gmail.com>
2023-02-19 14:37:41 -0600

    Testd inserting player

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ public class main
     public static void main(String [] args) throws Exception
     {
        Database db = new Database();
-       db.insertPlayer(420, "weed");
+       db.insertPlayer(2000, "y2k");
+       db.insertPlayer(3000, "y3k");
+
        db.closeConnection();
 
     }    
