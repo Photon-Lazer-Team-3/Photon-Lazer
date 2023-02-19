61a8f0b9e33dbf0fb69416030fdd7d5ed3d85bad
AlexJPrill <alex.j.prill@gmail.com>
2023-02-19 14:39:53 -0600

    Testing database

--- Main.java
+++ Main.java
@@ -4,8 +4,7 @@ public class main
     public static void main(String [] args) throws Exception
     {
        Database db = new Database();
-       db.insertPlayer(2000, "y2k");
-       db.insertPlayer(3000, "y3k");
+       
 
        db.closeConnection();
 
