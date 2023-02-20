f51d8bc46ce9651f736ccab8cb8f3145a774a728
Ben Fletcher <120226942+baf007@users.noreply.github.com>
2023-02-19 23:14:35 -0600

    Linked splash screen to main

--- Main.java
+++ Main.java
@@ -4,7 +4,21 @@ public class Main
 {
     public static void main(String [] args) throws Exception
     {
-       Database db = new Database();
+       CreateSplashScreen splashScreen = new CreateSplashScreen();
+		try 
+		{
+			// Make JWindow appear for 5 Seconds before Disappearing
+			// EDIT: Make JFrame appear for 3 seconds before Disappearing
+			Thread.sleep(3000);
+			
+			// Destroys and Cleans Up JFrame Window by O.S. (Note: Line 55 may also cause Program to Terminate if no other windows are available)
+			splashScreen.dispose();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+        Database db = new Database();
 
 
 
