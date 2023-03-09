35feead25eaa18f604657dd62df0b9018f4a644c
AlexJPrill <alex.j.prill@gmail.com>
2023-03-08 20:14:33 -0600

    function to add player to action screen

--- PlayActionScreen.java
+++ PlayActionScreen.java
@@ -28,8 +28,11 @@ public class PlayActionScreen extends JFrame
 	JFrame actionFrame;
 	
 	Dimension screenSize;
+
+	private Player redTeam[];
+	private Player greenTeam[];
 	
-	public PlayActionScreen()
+	public PlayActionScreen(PlayerEntreeScreen screen)
 	{
 		// Adds title to the frame
 		actionFrame = new JFrame("Play Action Terminal");
@@ -46,8 +49,45 @@ public class PlayActionScreen extends JFrame
 		
 		// Sets the windows background color to black
 		actionFrame.getContentPane().setBackground(Color.BLACK);
+
+		//Adds players from the entry screen to the Teams
+		redTeam = new Player[15];
+		greenTeam = new Player[15];
+		for(int i = 1; i < 30; i+=2)
+		{
+			if(!screen.redText[i].equals(""))
+			{
+				addRedPlayer(new Player(screen.redText[i]));
+			}
+			if(!screen.greenText[i].equals(""))
+			{
+				addGreenPlayer(new Player(screen.greenText[i]));
+			}
+		}
+		
+
 	}
 	
+	private void addGreenPlayer(Player player)
+	{
+		for(int i = 0; i < 15; i++)
+		{
+			if(greenTeam[i] == null)
+			{
+				greenTeam[i] = player;
+			}
+		}
+	}
+	private void addRedPlayer(Player player)
+	{
+		for(int i = 0; i < 15; i++)
+		{
+			if(redTeam[i] == null)
+			{
+				redTeam[i] = player;
+			}
+		}
+	}
 	// Testing the Play Action Screen
 	public static void main(String[] args)
 	{
