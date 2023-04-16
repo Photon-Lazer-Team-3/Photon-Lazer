50a73ff3c6ce5c4547be1d4160df851fa62ede4d
Ben Fletcher <120226942+baf007@users.noreply.github.com>
2023-04-15 19:16:53 -0500

    Cleaned up code

--- PlayerEntryScreen.java
+++ PlayerEntryScreen.java
@@ -25,23 +25,24 @@ import java.awt.Font;
 
 public class PlayerEntryScreen extends JFrame implements ActionListener
 {
-	JFrame entryFrame;
-	JTextField [] redText = new JTextField[30];
-	JTextField [] greenText = new JTextField[30];
-	boolean [] greenTextUpdate = new boolean[30];
-	boolean [] redTextUpdate = new boolean[30];
+	private JFrame entryFrame;
+	private ModifiedTextField [] redText = new ModifiedTextField[30];
+	private ModifiedTextField [] greenText = new ModifiedTextField[30];
+	private boolean [] greenTextUpdate = new boolean[30];
+	private boolean [] redTextUpdate = new boolean[30];
 	
-	JLabel [] redLabels = new JLabel[15];
-	JLabel [] greenLabels = new JLabel[15];
-	JLabel redLabelHeader, greenLabelHeader, timer;
+	private JLabel [] redLabels = new JLabel[15];
+	private JLabel [] greenLabels = new JLabel[15];
+	private JLabel redLabelHeader, greenLabelHeader, timer;
 	
-	JButton edit, start;
+	private JButton edit, start;
 
 	
 	// Added by Joseph Telford --> 2/18/2023 at 2:56PM
-	Dimension screenSize;
+	private Dimension screenSize;
+	Database db;
 	
-	public PlayerEntryScreen()
+	public PlayerEntryScreen() throws Exception
 	{
 		// Adds title to the frame
 		entryFrame = new JFrame("Player Entry Terminal");
@@ -59,30 +60,16 @@ public class PlayerEntryScreen extends JFrame implements ActionListener
 		int screenWidth = (int)screenSize.getWidth();
 		int screenHeight = (int)screenSize.getHeight();
 		entryFrame.setSize(screenWidth, screenHeight);
-		
-		// Creates red team name header
+
 		redLabelHeader = new JLabel("Red Team", SwingConstants.CENTER);
-		
-		int redLabelXPos = (screenWidth / 8);
-		int redLabelYPos = (screenHeight / 16) - 40;
-		
-		redLabelHeader.setBounds(redLabelXPos, redLabelYPos, 280, 40);
-		redLabelHeader.setForeground(Color.RED);
-		Border border = BorderFactory.createLineBorder(Color.WHITE, 1);
-		redLabelHeader.setBorder(border);
-		entryFrame.add(redLabelHeader);
-		
-		// Creates green team name header
 		greenLabelHeader = new JLabel("Green Team", SwingConstants.CENTER);
-		
+
+		int redLabelXPos = (screenWidth / 8);
 		int greenLabelXPos = ((11 * screenWidth) / 16);
-		int greenLabelYPos = (screenHeight / 16) - 40;
+		int labelYPos = (screenHeight / 16) - 40;
 		
-		greenLabelHeader.setBounds(greenLabelXPos, greenLabelYPos, 280, 40);
-		greenLabelHeader.setForeground(Color.GREEN);
-		Border border2 = BorderFactory.createLineBorder(Color.WHITE, 1);
-		greenLabelHeader.setBorder(border2);
-		entryFrame.add(greenLabelHeader);
+		createTeamHeader(redLabelHeader, redLabelXPos, labelYPos, Color.RED);
+		createTeamHeader(greenLabelHeader, greenLabelXPos, labelYPos, Color.GREEN);
 		
 		// Creates timer header
 		timer = new JLabel("30", SwingConstants.CENTER);
@@ -98,17 +85,11 @@ public class PlayerEntryScreen extends JFrame implements ActionListener
 		greenLabelHeader.setBorder(border3);
 		entryFrame.add(timer);
 		
-		// Creates and adds red team textboxes
-		createRedTextbox();
-		
-		// Creates and adds red team labels
-		createRedLabels();
-		
-		// Creates and adds green team textboxes
-		createGreenTextbox();
-		
-		// Creates and adds green team labels
-		createGreenLabels();
+		createTextBox(redText, redLabelXPos);
+		createTextBox(greenText, greenLabelXPos);
+
+		createLabels(redLabels, redLabelXPos - 35, Color.RED);
+		createLabels(greenLabels, greenLabelXPos + 315, Color.GREEN);
 		
 		// Adds edit button
 		editButton();
@@ -122,7 +103,17 @@ public class PlayerEntryScreen extends JFrame implements ActionListener
 		
 		// Sets the windows background color to black
 		entryFrame.getContentPane().setBackground(Color.BLACK);
-		int i = 1;
+		
+
+		db = new Database();
+	}
+
+	public void createTeamHeader(JLabel labelHeader, int labelXPos, int labelYPos, Color labelColor) {
+		labelHeader.setBounds(labelXPos, labelYPos, 280, 40);
+		labelHeader.setForeground(labelColor);
+		Border border = BorderFactory.createLineBorder(Color.WHITE, 1);
+		labelHeader.setBorder(border);
+		entryFrame.add(labelHeader);
 	}
 
 	public void timerUpdate()
@@ -144,105 +135,49 @@ public class PlayerEntryScreen extends JFrame implements ActionListener
 		}
 	}
 
-	public void createRedTextbox()
-	{
+	public void createTextBox(ModifiedTextField [] playerText, int playerXPos) {
 		// Added by Joseph Telford
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int screenWidth = (int)screenSize.getWidth();
 		int screenHeight = (int)screenSize.getHeight();
 		// Updated by Joseph Telford
-		int x = screenWidth / 8; // Initial x position
+		int x = playerXPos; // Initial x position
 		int y = screenHeight / 16; // Initial y position
 		
 		for (int i = 0; i < 30; i++)
 		{
-			redText[i] = new JTextField();
-			redText[i].setBounds(x, y, 140, 40);
-			redText[i].setEnabled(false);
+			playerText[i] = new ModifiedTextField(i);
+			//lksdjfklsdfj
+			playerText[i].setBounds(x, y, 140, 40);
+			playerText[i].setEnabled(false);
 
-			if (i % 2 == 0)
-			{
-				x = (screenWidth / 8) + 140;
+			if (i % 2 == 0) {
+				x += 140;
 			}
-			else
-			{
-				x = screenWidth / 8;
+			else {
+				x = playerXPos;
 				y += 40;
 			}
-			entryFrame.add(redText[i]);
+			entryFrame.add(playerText[i]);
 		}
 	}
 
-	public void createGreenTextbox()
-	{
+	public void createLabels(JLabel [] teamLabels, int playerXPos, Color teamColor) {
 		// Added by Joseph Telford
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int screenWidth = (int)screenSize.getWidth();
 		int screenHeight = (int)screenSize.getHeight();
 		// Updated by Joseph Telford
-		int x = (11 * screenWidth) / 16; // Initial x position
+		int x = playerXPos; // Initial x position
 		int y = screenHeight / 16; // Initial y position
 		
-		for (int i = 0; i < 30; i++)
-		{
-			greenText[i] = new JTextField();
-			greenText[i].setBounds(x, y, 140, 40);
-			greenText[i].setEnabled(false);
-
-			if (i % 2 == 0)
-			{
-				x = ((11 * screenWidth) / 16) + 140;
-			}
-			else
-			{
-				x = (11 * screenWidth) / 16;
-				y += 40;						// y += 35 was causing overlap issue on Windows
-			}
-			entryFrame.add(greenText[i]);
-		}
-	}
-
-	public void createRedLabels()
-	{
-		// Added by Joseph Telford
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int screenWidth = (int)screenSize.getWidth();
-		int screenHeight = (int)screenSize.getHeight();
-		// Updated by Joseph Telford
-		int x = (screenWidth / 8) - 35; // Initial x position
-		int y = screenHeight / 16; // Initial y position
-		
-		for (int i = 0; i < 15; i++)
-		{
+		for (int i = 0; i < 15; i++) {
 			int number = i + 1;
 			String playerNum = Integer.toString(number);
-			redLabels[i] = new JLabel(playerNum);
-			redLabels[i].setBounds(x, y, 15, 40);
-			redLabels[i].setForeground(Color.RED);
+			teamLabels[i] = new JLabel(playerNum);
+			teamLabels[i].setBounds(x, y, 15, 40);
+			teamLabels[i].setForeground(teamColor);
 			y += 40;
-			entryFrame.add(redLabels[i]);
-		}
-	}
-
-	public void createGreenLabels()
-	{
-		// Added by Joseph Telford
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int screenWidth = (int)screenSize.getWidth();
-		int screenHeight = (int)screenSize.getHeight();
-		// Updated by Joseph Telford
-		int x = ((11 * screenWidth) / 16) + 315; // Initial x position
-		int y = screenHeight / 16; // Initial y position
-		
-		for (int i = 0; i < 15; i++)
-		{
-			int number = i + 1;
-			String playerNum = Integer.toString(number);
-			greenLabels[i] = new JLabel(playerNum);
-			greenLabels[i].setBounds(x, y, 15, 40);
-			greenLabels[i].setForeground(Color.GREEN);
-			y += 40;
-			entryFrame.add(greenLabels[i]);
+			entryFrame.add(teamLabels[i]);
 		}
 	}
 
@@ -282,6 +217,45 @@ public class PlayerEntryScreen extends JFrame implements ActionListener
 		entryFrame.add(start);
 	}
 
+	//This will go over everything and check for updates and do other things
+	public void update() throws NumberFormatException, SQLException
+	{
+		for(int i = 0; i < 30; i+=2)
+		{
+			if(redText[i].isUpdated())
+			{
+				if(db.idExist(Integer.parseInt(redText[i].getText())))
+					{
+						redText[i+1].setText(db.getCodeName(Integer.parseInt(redText[i].getText())));
+					}
+					else
+					{
+						if(redText[i+1].isUpdated())
+						{
+							db.insertPlayer(Integer.parseInt(redText[i].getText()), redText[i+1].getText());
+						}
+					}
+			}
+		}
+		for(int i = 0; i < 30; i+=2)
+		{
+			if(greenText[i].isUpdated())
+			{
+				if(db.idExist(Integer.parseInt(greenText[i].getText())))
+					{
+						greenText[i+1].setText(db.getCodeName(Integer.parseInt(greenText[i].getText())));
+					}
+					else
+					{
+						if(greenText[i+1].isUpdated())
+						{
+							db.insertPlayer(Integer.parseInt(greenText[i].getText()), greenText[i+1].getText());
+						}
+					}
+			}
+		}
+	}
+
 	// When button is clicked allow input in text fields
 	public void actionPerformed(ActionEvent e)
 	{
@@ -300,21 +274,40 @@ public class PlayerEntryScreen extends JFrame implements ActionListener
 		if (e.getSource() == start)
 		{
 			timerUpdate();
-			PlayActionScreen actionScreen = new PlayActionScreen(this);
-			actionScreen.setVisible(true);
+			//PlayActionScreen actionScreen = new PlayActionScreen(this);
+			//actionScreen.setVisible(true);
 			entryFrame.dispose();
 		}
 	}
-		public String getGreenText(int i)
+	public String getGreenText(int i)
 	{
 		return greenText[i].getText();
+	}
+	public String getRedText(int i)
+	{
+		return redText[i].getText();
+	}
+
+	public void setRedText(int i, String text)
+	{
+		this.redText[i].setText(text);
+	}
+
+	public void setGreenText(int i, String text)
+	{
+		this.greenText[i].setText(text);
 	}
 
 	
 	
-	// // Testing the Player Entry Screen
-	// public static void main(String[] args)
-	// {
-		// new PlayerEntryScreen();
-	// }
+	// Testing the Player Entry Screen
+	public static void main(String[] args) throws Exception
+	{
+		PlayerEntryScreen screen = new PlayerEntryScreen();
+
+		while(true)
+		{
+			screen.update();
+		}
+	}
 }
